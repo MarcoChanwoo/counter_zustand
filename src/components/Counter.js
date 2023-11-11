9532a78b1e50037ddfd896766bec18c7e0da22f1
MarcoChanwoo <cwhong8087@gmail.com>
2023-11-11 11:27:25 +0900

    updated

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,9 +1,14 @@
-import React from "react";
-import { useCounterStore } from "../store";
+import React from 'react'
+import { useCounterStore } from '../store'
 
 const Counter = () => {
-  const { count, increment } = useCounterStore;
-  return <div>Counter</div>;
-};
+  const {count, increment}=useCounterStore()
+  return (
+    <div>
+      <p>{count}</p>
+      <button onClick={}>one up</button>
+    </div>
+  )
+}
 
-export default Counter;
+export default Counter
