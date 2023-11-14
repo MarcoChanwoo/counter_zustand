da116d02c2e4ac60e76b43a205e84c395cf992a0
MarcoChanwoo <cwhong8087@gmail.com>
2023-11-14 11:55:00 +0900

    updated

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 const TodoList = () => {
+  const [todoValue, setTodoValue] = useState("");
   return (
     <div>
       <h3>Todo App</h3>
+      <form>
+        <input type="text" id="new-todo" name="newTodo" value={todoValue} />
+      </form>
     </div>
   );
 };
