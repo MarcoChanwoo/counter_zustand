9d8d9825b701ef3a31d180dd089837687c335484
MarcoChanwoo <cwhong8087@gmail.com>
2023-11-16 16:01:49 +0900

    updated

--- src/store/useTodoStore.js
+++ src/store/useTodoStore.js
@@ -2,36 +2,39 @@ import { create } from "zustand";
 import { persist } from "zustand/middleware";
 
 export const useTodoStore = create(
-  persist((set) => ({
-    todos: [],
-    addTodo: (todoText) =>
-      set((state) => ({
-        todos: [
-          ...state.todos,
-          {
-            text: todoText,
-            id: getId(),
-            isCompleted: false,
-          },
-        ],
-      })),
-    deleteTodo: (todoId) =>
-      set((state) => ({
-        todos: state.todos.filter((todo) => todo.id !== todoId),
-      })),
-    completeTodo: (todoId) =>
-      set((state) => ({
-        todos: state.todos.map((todo) => {
-          if (todo.id === todoId) {
-            return {
-              ...todo,
-              isCompleted: true,
-            };
-          }
-          return todo;
-        }),
-      })),
-  }))
+  persist(
+    (set) => ({
+      todos: [],
+      addTodo: (todoText) =>
+        set((state) => ({
+          todos: [
+            ...state.todos,
+            {
+              text: todoText,
+              id: getId(),
+              isCompleted: false,
+            },
+          ],
+        })),
+      deleteTodo: (todoId) =>
+        set((state) => ({
+          todos: state.todos.filter((todo) => todo.id !== todoId),
+        })),
+      completeTodo: (todoId) =>
+        set((state) => ({
+          todos: state.todos.map((todo) => {
+            if (todo.id === todoId) {
+              return {
+                ...todo,
+                isCompleted: true,
+              };
+            }
+            return todo;
+          }),
+        })),
+    }),
+    { name: "todo" }
+  )
 );
 
 let id = 0;
