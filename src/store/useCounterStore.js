65556936e9440ae89defac11a237eb8163cf8cbd
MarcoChanwoo <cwhong8087@gmail.com>
2023-11-16 16:12:51 +0900

    updated

--- src/store/useCounterStore.js
+++ src/store/useCounterStore.js
@@ -1,16 +1,9 @@
 import { create } from "zustand";
 import { devtools, persist } from "zustand/middleware";
 
-export const useCounterStore = create(
-  persist(
-    devtools(
-      (set) => ({
-        count: 1,
-        increment: () => set((state) => ({ count: state.count + 1 })),
-        reset: () => set({ count: 1 }),
-        setNumber: (number) => set({ count: number }),
-      }),
-      { name: "counter" }
-    )
-  )
-);
+let counterStore = (set) => ({
+  count: 1,
+  increment: () => set((state) => ({ count: state.count + 1 })),
+  reset: () => set({ count: 1 }),
+  setNumber: (number) => set({ count: number }),
+});
