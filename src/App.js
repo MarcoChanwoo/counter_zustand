f103fef6e44a023d082dc86bab492096148a33ff
MarcoChanwoo <cwhong8087@gmail.com>
2023-11-19 23:22:28 +0900

    updated

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import logo from "./logo.svg";
 import "./App.css";
 import Counter from "./components/Counter";
 import TodoList from "./components/TodoList";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {}, []);
+
   return (
     <div className="App">
       <header className="App-header">
