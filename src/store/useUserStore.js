4066e4f69db9c7ae086a387175f2d865fc16f063
MarcoChanwoo <cwhong8087@gmail.com>
2023-11-19 23:17:45 +0900

    updated

--- src/store/useUserStore.js
+++ src/store/useUserStore.js
@@ -4,5 +4,6 @@ export const useUserStore = create((set) => ({
   user: {},
   fetch: async (id) => {
     const path = `https://jsonplaceholder.typicode.com/users/${id}`;
+    const response = await fetch(path);
   },
 }));
