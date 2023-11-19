56a89e8a4348085a329512cdc659164b232fe9a0
MarcoChanwoo <cwhong8087@gmail.com>
2023-11-19 23:23:07 +0900

    updated

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import "./App.css";
 import Counter from "./components/Counter";
 import TodoList from "./components/TodoList";
 import { useEffect } from "react";
+import { useUserStore } from "./store/useUserStore";
 
 function App() {
+  const { fetch, user } = useUserStore();
   useEffect(() => {}, []);
 
   return (
